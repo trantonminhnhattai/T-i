fc7ba7cd912917768e86d76d6d64c8ffd4ba1126
trantonminhnhattai <taimodgame100@gmail.com>
2025-03-22 11:21:23 +0700

    Create Taimodgame.lua

---
+++ Taimodgame.lua
@@ -0,0 +1,10 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruits
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trantonminhnhattai/Flytaimodgame/refs/heads/main/Flytaimodgame.lua"))()
+  --Fly
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/trantonminhnhattai/Scriptonghoptaimodgame/refs/heads/main/Taimodgame")))() 
+  --Blox Fruits
+end
